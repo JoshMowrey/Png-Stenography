0a1b35c4723c20f0aa58a1c623325bdb89b19537
root <root@Mowrey0316>
2024-11-25 15:04:28 -0800

    ihdr parsing & ihdr struct

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
-#![allow(non_snake_case)]
 #![allow(unused_parens)]
-//use std::env;
+#![allow(dead_code)]
 use std::fs;
 
-fn main() {
-    let FilePath = "in.png";
+struct IHDR {
+    header: [u8; 4],
+    width: u32,
+    height: u32,
+    bit_depth: u8,
+    color_type: u8,
+    compression_method: u8,
+    filter_method: u8,
+    interlace_method: u8,
+}
 
-    let mut data: Vec<u8> = fs::read(FilePath).unwrap();
+
+fn main() {
+    let file_path = "in.png";
+
+    let mut data: Vec<u8> = fs::read(file_path).unwrap();
     let mut idat_start = 0;
 
     for (i, iter) in data.clone().into_iter().enumerate() {
@@ -27,6 +38,8 @@ fn main() {
         }
 
     }
+    println!("Print tRNS:  ");
+    print_ihdr(ihdr_data());
     println!("Number of Idat Chunks:  {}", num_of_idat);
     data.extend(b"Hello World");
     let _ = fs::write("out.png", data.clone()).unwrap();
@@ -35,30 +48,68 @@ fn main() {
              data[idat_start + 3] as char);
 }
 
-fn find_tRNS() -> String {
-    let types: Vec[u8] = [];
-    for (i, iter) in data.clone().into_iter().enumerate() {
+fn ihdr_data() -> Vec<u8> {
+    
+    let file_path = "in.png";
+
+    let file_data: Vec<u8> = fs::read(file_path).unwrap();
+    let mut ihdr_data: Vec<u8> = vec![];
+    for (i, iter) in file_data.clone().into_iter().enumerate() {
         if (iter == 73) {
-            if (data[i+1] == 68 && data[i+2] == 65 && data[i+3] == 84){
-                types.push(data[i+13]);
-            }
-        }
-    }
-    let current_tRNS: Vec[u8] = [];
-    for (i, iter) in data.clone().into_iter().enumerate() {
-        if (iter == 116) {
-            if (data[i+1] == 82 && data[i+2] == 78 && data[i+3] == 83){
-                for j in range(i+4, i+15) {
-                    current_tRNS.push(data[j]);
+            if (file_data[i+1] == 72 && file_data[i+2] == 
+                68 && file_data[i+3] == 82) {
+
+                for j in i+4..i+17 {
+                    ihdr_data.push(file_data[j]);
                 }
             }
         }
     }
-    for (i, iter) 
+    println!("IHDR len: {}", ihdr_data.len());
+    return ihdr_data;
 }
 
-fn make_IHDR(String: s){
-    let mut Buffer: Vec<u8> = [73, 72, 68, 82];
-    
+fn make_ihdr(){
+        
 }
 
+fn print_vecu8(data: Vec<u8>){
+    for iter in data {
+        print!(" {},", iter);
+    }
+}
+
+fn print_ihdr(data: Vec<u8>) -> IHDR { 
+    println!("Image Width: ");
+    for i in 0..4 {
+        print!("{} ", data[i]);
+    }
+    println!("\nImage Depth: ");
+    for i in 4..8 {
+        print!("{} ", data[i]);
+    }
+    println!("\nBit Depth: {}\nColor Type: {}\nCompression Type: {}\nFilter Method: {}\nInterlace Method {}", 
+             data[8], data[9], data[10], data[11], data[12]);
+    let mut width_pre_transmute: [u8; 4] = [0; 4];
+    width_pre_transmute.clone_from_slice(&data[..4]);
+    let height_pre_transmute: [u8; 4] = [0; 4];
+    width_pre_transmute.clone_from_slice(&data[4..4]);
+    let width = unsafe {
+        std::mem::transmute::<[u8; 4], u32>(width_pre_transmute)
+    };
+    let height = unsafe {
+        std::mem::transmute::<[u8; 4], u32>(height_pre_transmute)
+    };
+    let ihdr = IHDR {
+        header: [73, 72, 68, 82],
+        width: width,
+        height: height,
+        bit_depth: data[8],
+        color_type: data[9],
+        compression_method: data[10],
+        filter_method: data[11],
+        interlace_method: data[12],
+    };
+    return ihdr;
+
+}
